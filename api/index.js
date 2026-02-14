6468dfd91079022656c84e14322a9e6364d238bc
HeshamAltaweel <109346029+HeshamAltaweel@users.noreply.github.com>
2026-02-14 06:53:36 +0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Main route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// أي منفذ Vercel يعطيه
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
